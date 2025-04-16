3ac34a0abe1cf776c2c47413952a3ec42e9b8535
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-17 01:22:07 +0530

    completed 1 to N problem

---
+++ 219432_A.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    long long int num;
+    std::cin >> num;
+    std::cout << num;
+    for (long long int i = 1; i <= num; i++)
+    {
+        std::cout << i << "\n";
+    }
+    exit(EXIT_SUCCESS);
+}

ecd3f6d6e0be94ebf8149dd1ddc96f5b137e3c0d
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-17 03:09:48 +0530

    Multiplication table

---
+++ 219432_F.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char const *argv[])
+{
+    uint8_t x;
+    std::cin >> x;
+    for (size_t i = 1; i <= 12; i++)
+        std::cout << x << " * " << i << " = " << x * i << "\n";
+    exit(EXIT_SUCCESS);
+}

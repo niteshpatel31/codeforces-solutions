dff3643897879be0214f07139a6af7b252e9c100
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-18 12:43:51 +0530

    One Prime

--- 219432_H.cpp
+++ 219432_H.cpp
@@ -1,12 +1,55 @@
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 
-int main(int argc, char const *argv[])
-{
-    int x;
-    std::cin >> x;
-    for (size_t i = 2; i < x; i++)
+// #define ll long long int
+// bool find_prime(ll num)
+// {
+//     if (num <= 2)
+//         return false;
+//     for (int i = 2; i < num / 2; i++)
+//     {
+//         if (num % i == 0)
+//             return false;
+//     }
+//     return true;
+// }
+
+// int main(int argc, char const *argv[])
+// {
+//     ll x;
+//     std::cin >> x;
+//     std::cout << ((find_prime(x) ? "YES" : "NO"));
+//     exit(EXIT_SUCCESS);
+// }
+// https://codeforces.com/group/MWSDmqGsZm/contest/219432/problem/H
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+typedef long long int ll;
+bool isprime4(ll n)
+{ // O( sqrt(n)
+    if (n == 2)
+        return true;
+
+    if (n < 2 || n % 2 == 0)
+        return false;
+
+    for (ll i = 3; i * i <= n; i += 2)
     {
+        if (n % i == 0)
+            return false;
     }
+    return true;
+}
 
-    exit(EXIT_SUCCESS);
+int main()
+{
+    ll p = 0;
+    cin >> p;
+    std::cout << ((isprime4(p)) ? "YES" : "NO");
+
+    return 0;
 }

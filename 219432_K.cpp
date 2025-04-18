53c6a227d969b0e010d7b78cb20bcf225d9e0856
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-18 17:13:31 +0530

    Divisors

--- 219432_K.cpp
+++ 219432_K.cpp
@@ -4,11 +4,20 @@ int main(int argc, char const *argv[])
 {
     int num;
     std::cin >> num;
-    for (int i = 0; i <= num; i++)
+    /*
+     for (int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+                std::cout << i << "\n";
+        }
+    */
+
+    for (int i = 1; i <= num / 2; i++)
     {
         if (num % i == 0)
             std::cout << i << "\n";
     }
+    std::cout << num << "\n";
 
     exit(EXIT_SUCCESS);
 }

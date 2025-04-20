03dbd294b593991d078f3174b9185a123608af18
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-20 22:21:20 +0530

    Two Numbers

--- 219158_H.cpp
+++ 219158_H.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 
-void round(short x, short y)
+void round(long x, long y)
 {
     float round = (float)x / y;
     std::cout << "floor " << x << " / " << y << " = " << int(round) << "\n";
-    std::cout << "ceil " << x << " / " << y << " = " << int(round) + 1 << "\n";
+    std::cout << "ceil " << x << " / " << y << " = " << ((round == 1.0f) ? int(round) : int(round) + 1) << "\n";
     std::cout << "round " << x << " / " << y << " = " << ((round - int(round) >= .5f) ? int(round) + 1 : int(round)) << "\n";
     return;
 }
 
 int main(int argc, char const *argv[])
 {
-    short x, y;
+    long x, y;
     std::cin >> x >> y;
     round(x, y);
     exit(EXIT_SUCCESS);

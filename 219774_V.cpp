b7bca324118dd8d0f53597d998425e03d2f30952
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-04-28 23:57:56 +0530

    Frequency Array

--- 219774_V.cpp
+++ 219774_V.cpp
@@ -3,9 +3,9 @@
 int main(int argc, char const *argv[])
 {
     // std::ios::sync_with_stdio(false);
-    unsigned short m, n, idx{0};
+    unsigned int m, n;
     std::cin >> n >> m;
-    unsigned int freq[m + 1] = {0};
+    unsigned int freq[100001] = {0};
     for (int i = 0; i < n; i++)
     {
         unsigned int buffer{0};

8b117b61b420d7002167699214c4d8a38a769a0b
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-05-08 05:15:42 +0530

    Finding Minimums

--- Contest#2/326907_C.cpp
+++ Contest#2/326907_C.cpp
@@ -12,19 +12,17 @@ int main(int argc, char const *argv[])
 {
     long long size, k;
     std::cin >> size >> k;
-    long long counter{0}, num, min{LONG_MAX}, s{size};
+    long long counter{0}, num, min{LONG_MAX};
     while (size--)
     {
         std::cin >> num;
         min = _min(num, min);
-        // std::cout << min;
         if (size == 0 || ++counter == k)
         {
             std::cout << min << " ";
             min = LONG_MAX;
             counter = 0;
         }
-        // std::cout << " : " << counter << std::endl;
     }
     exit(EXIT_SUCCESS);
 }

fb0cc4879ad9e6eec373cfa807203947451ddb8e
niteshpatel31 <cse22_niteshkumarpatel@ggct.co.in>
2025-05-08 07:35:07 +0530

    Hady Rides the Train & updated tasks.json

--- Contest#2/326907_E.cpp
+++ Contest#2/326907_E.cpp
@@ -3,28 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-    ull seat, row{0}, col{0};
-    // ull arr[8][2] = {{0, 0}, {1, 1}, {2, 2}, {3, 3}, {4, 3}, {5, 2}, {6, 1}, {7, 0}};
+    ull seat;
     std::cin >> seat;
-    // for (int i = 0; i < 8; i++)
-    // {
-    //     for (int j = 0; j < 2; j++)
-    //     {
-    //         std::cout << arr[i][j] << " ";std::cout << arr[col][1] << " " << arr[row][0];
-    //     }
-    //     std::cout << "\n";
-    // }
-    row = seat / 4;
-    if (row % 2 == 0)
-    {
-        col = seat % 4;
-        std::cout << row << " " << col;
-    }
-    else
-    {
-        col = 3 - (seat % 4);
-        std::cout << row << " " << col;
-    }
-
+    std::cout << seat / 4 << " " << ((seat / 4 % 2 == 0) ? (seat % 4) : 3 - (seat % 4));
     exit(EXIT_SUCCESS);
 }
